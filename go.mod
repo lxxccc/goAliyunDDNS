72132b0d7c657f4a034f86c72bf03598b54fde57
lxxccc <lxxccc@outlook.com>
2019-09-09 15:57:46 +0800

    支持mod模式，添加多平台编译结果

---
+++ go.mod
@@ -0,0 +1,12 @@
+module lxxccc.top/tools/goAliyunDDNS
+
+go 1.12
+
+require (
+	github.com/aliyun/alibaba-cloud-sdk-go v0.0.0-20190905061812-fecb5b8dfc49
+	github.com/denverdino/aliyungo v0.0.0-20190822085226-26b766f0dfd5
+	golang.org/x/tools v0.0.0-20190328211700-ab21143f2384
+	lxxccc.top/Library/ToolkitsGo v0.1.7
+)
+
+replace lxxccc.top/Library/ToolkitsGo v0.1.7 => home.lxxccc.top/Library/ToolkitsGo.git v0.1.7
---
+++ goAliyunDDNS.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"github.com/denverdino/aliyungo/dns"
+	"golang.org/x/tools/go/ssa/interp/testdata/src/fmt"
+	"lxxccc.top/Library/ToolkitsGo/config/json"
+	"lxxccc.top/Library/ToolkitsGo/network"
+)
+
+// ConfigInfo 定义域名相关配置信息
+type ConfigInfo struct {
+	AccessKeyID     string
+	AccessKeySecret string
+	DomainName      string
+	RR              string
+}
+
+func main() {
+	// 读取配置文件信息
+	config := new(ConfigInfo)
+	json.ReadJSONConfigToStruct("config.json", &config)
+	// 获取公网IP信息
+	publicIP, err := network.GetPublicIPUseDnspod()
+	fmt.Println("当前公网IP：" + publicIP)
+
+	// 连接阿里云服务器，获取DNS信息
+	client := dns.NewClient(config.AccessKeyID, config.AccessKeySecret)
+	client.SetDebug(false)
+	domainInfo := new(dns.DescribeDomainRecordsArgs)
+	domainInfo.DomainName = config.DomainName
+	oldRecord, err := client.DescribeDomainRecords(domainInfo)
+	if err != nil {
+		fmt.Println("链接错误")
+		return
+	}
+
+	var exsitRecordID string
+	for _, record := range oldRecord.DomainRecords.Record {
+		if record.DomainName == config.DomainName && record.RR == config.RR {
+			if record.Value == publicIP {
+				fmt.Println("当前配置解析地址与公网IP相同，不需要修改。")
+				return
+			}
+			exsitRecordID = record.RecordId
+		}
+	}
+
+	if 0 < len(exsitRecordID) {
+		// 有配置记录，则匹配配置文件，进行更新操作
+		updateRecord := new(dns.UpdateDomainRecordArgs)
+		updateRecord.RecordId = exsitRecordID
+		updateRecord.RR = config.RR
+		updateRecord.Value = publicIP
+		updateRecord.Type = dns.ARecord
+		rsp := new(dns.UpdateDomainRecordResponse)
+		rsp, err := client.UpdateDomainRecord(updateRecord)
+		if nil != err {
+			fmt.Println("修改解析失败", err)
+		} else {
+			fmt.Println("修改解析成功", rsp)
+		}
+	} else {
+		// 没有找到配置记录，那么就新增一个
+		newRecord := new(dns.AddDomainRecordArgs)
+		newRecord.DomainName = config.DomainName
+		newRecord.RR = config.RR
+		newRecord.Value = publicIP
+		newRecord.Type = dns.ARecord
+		rsp := new(dns.AddDomainRecordResponse)
+		rsp, err = client.AddDomainRecord(newRecord)
+		if nil != err {
+			fmt.Println("添加DNS解析失败", err)
+		} else {
+			fmt.Println("添加DNS解析成功", rsp)
+		}
+	}
+}
